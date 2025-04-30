cc87c04b9616925753abd48e0ba6c47b3f44d90d
Mame <34818226+MameMame777@users.noreply.github.com>
2025-04-30 20:15:04 +0900

    reconstruct of 8bit bmp failure in RLE. Fixes #11

--- include/BmpBaseDataProvider.h
+++ include/BmpBaseDataProvider.h
@@ -3,7 +3,6 @@
 
 #include "DataProvider.h"
 #include "bmpbase.h"
-
 class BmpBaseDataProvider : public DataProvider {
 private:
     BmpBase bmpBase; 
@@ -23,11 +22,17 @@ public:
         //Convert  BitmapFileHeader to byte array 
         const auto& fileHeader = bmpBase.getFileHeader();
         headerData.insert(headerData.end(), reinterpret_cast<const uint8_t*>(&fileHeader), 
-                          reinterpret_cast<const uint8_t*>(&fileHeader) + sizeof(fileHeader));
+                            reinterpret_cast<const uint8_t*>(&fileHeader) + sizeof(fileHeader));
         //Convert BitmapInfoHeader to byte array
         const auto& infoHeader = bmpBase.getInfoHeader();
         headerData.insert(headerData.end(), reinterpret_cast<const uint8_t*>(&infoHeader), 
-                          reinterpret_cast<const uint8_t*>(&infoHeader) + sizeof(infoHeader));
+                            reinterpret_cast<const uint8_t*>(&infoHeader) + sizeof(infoHeader));
+        //Convert ColorPalette to byte array
+        if (bmpBase.getInfoHeader().biBitCount == 8) {
+            const auto& colorPalette = bmpBase.getColorPalette();
+            headerData.insert(headerData.end(), reinterpret_cast<const uint8_t*>(colorPalette.data()), 
+                              reinterpret_cast<const uint8_t*>(colorPalette.data()) + colorPalette.size() * sizeof(BitmapColorPalette));
+        }
         return headerData;
     }
 };
